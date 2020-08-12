e826d3d1fe58718096c1495837c0d77b6a934861
Bob Robey <brobey@lanl.gov>
2020-08-11 22:06:30 -0600

    Container fixes

--- dockertest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-docker build --no-cache -t chapter14 .
-#docker run -it --entrypoint /bin/bash chapter14
-docker build --no-cache -f Dockerfile.Ubuntu20.04 -t chapter14 .
-#docker run -it --entrypoint /bin/bash chapter14
-docker build --no-cache -f Dockerfile.debian -t chapter14 .
-#docker run -it --entrypoint /bin/bash chapter14
